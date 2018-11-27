34efb360d5f62af3a4a19f709d43aa6e4e156d2b
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-11-27 08:29:48 +0000

    Add Provider and Qualification entities

---
+++ Dfc.Coursedata.Enrichment.Data/Entities/Provider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Dfc.Coursedata.Enrichment.Data.Entities
+{
+    public class Provider
+    {
+        public string Id { get; set; }
+        public string ProviderName { get; set; }
+        public IEnumerable<Qualification> Qualifications { get; set; }
+    }
+}
---
+++ Dfc.Coursedata.Enrichment.Data/Entities/Qualification.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfc.Coursedata.Enrichment.Data.Entities
+{
+    public class Qualification
+    {
+        public string Id { get; set; }
+        public string LarsTitle { get; set; }
+    }
+}
