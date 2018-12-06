da2ae43b5073a2ef6249608f7c20f4c982d978a4
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-12-06 12:59:31 +0000

    Retrieve from csv

--- Dfc.Coursedata.Enrichment.Importer/Data/ProviderCourseData.cs
+++ Dfc.Coursedata.Enrichment.Importer/Data/ProviderCourseData.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using CsvHelper;
 using Dfc.Coursedata.Enrichment.Importer.Entities;
+using Dfc.Coursedata.Enrichment.Importer.Entities.CSV;
 using Dfc.Coursedata.Enrichment.Importer.Interfaces;
-using Dfc.ProviderPortal.Providers;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Options;
 using UKRLP.Storage;
+using Provider = Dfc.ProviderPortal.Providers.Provider;
 
 namespace Dfc.Coursedata.Enrichment.Importer.Data
 {
@@ -18,6 +20,7 @@ namespace Dfc.Coursedata.Enrichment.Importer.Data
         static HttpClient _client = new HttpClient();
         private readonly string UrlGetAllProviders;
         private readonly string UrlCode;
+        private readonly string _csvFolder = @"C:\CareersServiceDocuments\WalsallCollege\TrimmedVersion2\";
 
         public ProviderCourseData(IOptions<UkrlpSettings> ukrlpSettings)
         {
@@ -48,6 +51,57 @@ namespace Dfc.Coursedata.Enrichment.Importer.Data
             var csvReader = new CsvReader(reader);
             var records = csvReader.GetRecords<ILR>();
             return records;
+        }
+
+
+        public Entities.CSV.Provider GetProvider()
+        {
+            TextReader reader = new StreamReader(_csvFolder + "Provider.csv");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<Entities.CSV.Provider>();
+
+            var providers = records as Entities.CSV.Provider[] ?? records.ToArray();
+            return providers.ToList().First();
+        }
+
+        public List<CourseDetail> GetCourseDetails()
+        {
+            TextReader reader = new StreamReader(_csvFolder + "CourseDetail.csv");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<CourseDetail>();
+
+            var courseDetails = records as CourseDetail[] ?? records.ToArray();
+            return courseDetails.ToList();
+        }
+
+        public List<Venue> GetVenues()
+        {
+            TextReader reader = new StreamReader(_csvFolder + "Venue.csv");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<Venue>();
+
+            var venues = records as Venue[] ?? records.ToArray();
+            return venues.ToList();
+        }
+
+        public List<Qualification> GetQualifications()
+        {
+            TextReader reader = new StreamReader(_csvFolder + "Qualification.csv");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<Qualification>();
+
+            var qualifications = records as Qualification[] ?? records.ToArray();
+            return qualifications.ToList();
+        }
+
+        public List<Opportunity> GetOpportunities()
+        {
+            TextReader reader = new StreamReader(_csvFolder + "Opportunity.csv");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<Opportunity>();
+
+            var opportunities = records as Opportunity[] ?? records.ToArray();
+            return opportunities.ToList();
         }
 
         public class TraceWriterStub : TraceWriter
